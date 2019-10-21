a2558be9f6020c8355489edd3d19d449054930b3
WojciechWeg <wojtek.weg@gmail.com>
2019-10-21 19:03:22 +0200

    tests declarations

---
+++ src/test/java/com/wojtek/api/StarWarsReportGeneratorAPI/controllers/ReportControllerTest.java
@@ -0,0 +1,68 @@
+package com.wojtek.api.StarWarsReportGeneratorAPI.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReportControllerTest {
+
+    @Test
+    void getListOfReports() {
+    }
+
+    @Test
+    void getListOfReportsThrowNotFound(){
+
+    }
+
+    @Test
+    void getReport() {
+    }
+
+    @Test
+    void getReportThrowInvalidArgument(){}
+
+    @Test
+    void getReportThrowNotFound(){}
+
+    @Test
+    void deleteAllReports() {
+    }
+
+    @Test
+    void deleteAllReportsThrowNotFound(){}
+
+    @Test
+    void deleteReportById() {
+    }
+
+    @Test
+    void deleteReportByIdThrowInvalidArgument(){}
+
+    @Test
+    void deleteReportByIdThrowNotFound(){}
+
+    @Test
+    void createNewReport() {
+    }
+
+    @Test
+    void createNewReportThrowPlanetNotFound(){
+
+    }
+
+    @Test
+    void createNewReportThrowPlanetHasNoResidents(){
+
+    }
+
+    @Test
+    void createNewReportThrowNoSuchResident(){}
+
+    @Test
+    void creteNewReportThrowNoFilmsForGivenCharacter(){}
+
+    @Test
+    void createNewReportThrowInvalidArgument(){}
+
+}
---
+++ src/test/java/com/wojtek/api/StarWarsReportGeneratorAPI/services/ReportServiceTest.java
@@ -0,0 +1,28 @@
+package com.wojtek.api.StarWarsReportGeneratorAPI.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReportServiceTest {
+
+    @Test
+    void createReport() {
+    }
+
+    @Test
+    void createReportThrowPlanetNotFound(){
+
+    }
+
+    @Test
+    void createReportThrowPlanetHasNoResidents(){
+
+    }
+
+    @Test
+    void createReportThrowNoSuchResident(){}
+
+    @Test
+    void creteReportThrowNoFilmsForGivenCharacter(){}
+}
