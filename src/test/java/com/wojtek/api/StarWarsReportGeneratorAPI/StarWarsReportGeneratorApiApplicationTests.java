57b9b817d7bf5e0db2af919c3436b23798226bc8
WojciechWeg <wojtek.weg@gmail.com>
2019-10-21 14:57:40 +0200

    init

---
+++ src/test/java/com/wojtek/api/StarWarsReportGeneratorAPI/StarWarsReportGeneratorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wojtek.api.StarWarsReportGeneratorAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarWarsReportGeneratorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
